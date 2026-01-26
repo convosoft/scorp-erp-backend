86172f823dc25cea006bdef01cd74abc622607e6
azmatrasheedchohan <azmatchohan01@gmail.com>
2026-01-26 18:27:08 +0500

    elt table name

--- app/Models/ELTRequirement.php
+++ app/Models/ELTRequirement.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class ELTRequirement extends Model
 {
     use HasFactory;
+    protected $table = 'elt_requirements';
 
     protected $fillable = [
         'name',
