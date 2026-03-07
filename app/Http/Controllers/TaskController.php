4fa8af8f4a161e4acdfce25ef992eddc80fc92fa
azmatrasheedchohan <azmatchohan01@gmail.com>
2026-03-07 07:06:14 +0500

    sss

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -355,8 +355,8 @@ class TaskController extends Controller
             if ($request->filled('tasks_type_status')) {
                 $status = $request->tasks_type_status;
                 if ($status == '1') {
-                    $tasksQuery->where('deal_tasks.tasks_type_status', "1");
-                            //  ->where('deal_tasks.status', 1);
+                    $tasksQuery->where('deal_tasks.tasks_type_status', "1")
+                              ->where('deal_tasks.status', 1);
                 } elseif ($status == '2') {
                     $tasksQuery->where('deal_tasks.tasks_type_status', "2");
                 } else {
