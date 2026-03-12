a34360326df5614f32105ed45894324285a7b399
azmatrasheedchohan <azmatchohan01@gmail.com>
2026-03-12 11:53:14 +0500

    test

--- app/Models/CompanyPermission.php
+++ app/Models/CompanyPermission.php
@@ -11,10 +11,18 @@ class CompanyPermission extends Model
     use HasFactory;
     protected $table = 'company_permission';
 
+     protected $fillable = [
+        'user_id',
+        'permitted_company_id',
+        'active',
+        'created_by',
+    ];
+
+
     public function user()
     {
         return $this->belongsTo(User::class, 'company_id');
     }
 
-   
+
 }
