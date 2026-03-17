88f46a8b10498b8748f11ef80d76f6bfba5a94f8
azmatrasheedchohan <azmatchohan01@gmail.com>
2026-03-17 11:42:52 +0500

    select

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1261,7 +1261,7 @@ class TaskController extends Controller
                 'brand:id,name',
                 'region:id,name',
                 'branch:id,name'
-            ])->find($task->assigned_to);
+            ])->select('id', 'name','brand_id','branch_id','region_id')->find($task->assigned_to);
 
         return response()->json([
             'status' => 'success',
