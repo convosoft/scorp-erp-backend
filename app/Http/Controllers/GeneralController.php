d6e36c97fe3d558d05daf82a2f4c42503bd792a4
azmatrasheedchohan <azmatchohan01@gmail.com>
2026-05-14 15:54:40 +0500

    S

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -134,7 +134,7 @@ class GeneralController extends Controller
                 ->toArray();
 
             if (count($regions) === 1) {
-                $regions = ['0' => 'Select Region'] + $regions;
+                $regions = ['0' => 'Please Select Region'] + $regions;
             }
 
             // Return JSON response with regions
