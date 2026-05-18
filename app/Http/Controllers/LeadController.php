a6902f302a910910da85867a4449b86f717fe41a
azmatrasheedchohan <azmatchohan01@gmail.com>
2026-05-18 15:20:20 +0500

    download leads

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -479,7 +479,7 @@ class LeadController extends Controller
         }
 
         // ✅ CSV Export - No joins needed!
-        if ($request->input('download_csv')) {
+        if ($request->input('download_csv') == 0) {
             $download_csv = $leadsQuery->where('is_converted', 0)->get();
 
             $headers = [
